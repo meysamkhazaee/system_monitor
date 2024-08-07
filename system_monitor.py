da1419c6fa31b5c8d2c916e0c4984ac06f869d83
m.khazaee <meysam.khazai@peykasa.ir>
2024-08-07 18:50:08 +0330

    add i/o network monitoring guages

--- system_monitor.py
+++ system_monitor.py
@@ -11,6 +11,8 @@ system_cpu_usage_gauge = Gauge('system_cpu_usage_percent', 'Percentage of CPU us
 process_cpu_usage_gauge = Gauge('process_cpu_usage', 'CPU Usage of a specific process', ['process_name'])
 process_memory_usage_gauge = Gauge('process_memory_usage_', 'Memory Usage of a specific process in MB', ['process_name'])
 process_memory_percentage_usage_gauge = Gauge('process_memory_percentage_usage_', 'Memory Usage Percentage of a specific process', ['process_name'])
+net_io_sent_guage = Gauge('net_io_sent', 'KiloBytes sent over network')
+net_io_recv_guage = Gauge('net_io_recv', 'KiloBytes received over network')
 
 def find_process_by_name(name):
     for proc in psutil.process_iter(['pid', 'name', 'exe', 'cpu_percent', 'memory_info']):
@@ -43,11 +45,12 @@ def get_system_usage():
 if __name__ == '__main__':
     
     if len(sys.argv) != 3:
-        print("Usage: python system_monitor.py <process_name> <port_prometheus>")
-        sys.exit(1)
+        process_name = input("Enter process name (or press Enter for 'PAPERServer'): ") or 'PAPERServer'
+        port = int(input("Enter port (or press Enter for 9990): ") or 9990)
 
-    process_name = sys.argv[1]
-    port = int(sys.argv[2])
+    else:
+        process_name = sys.argv[1]
+        port = int(sys.argv[2])
 
     start_http_server(port)
 
@@ -56,6 +59,13 @@ if __name__ == '__main__':
     system_memory_volume_gauge.set(total_memory)
     system_cpu_cores_gauge.set(total_cores)
 
+    # Get the network interface objects
+    net_if_addrs = psutil.net_if_addrs()
+    net_ifaces = [k for k, v in net_if_addrs.items() if v]
+
+    # Initialize the network I/O stats dictionary
+    net_io_stats = {iface: {'bytes_sent': 0, 'bytes_recv': 0} for iface in net_ifaces}
+    
     while True:
         
         # Get current system usage
@@ -76,10 +86,36 @@ if __name__ == '__main__':
                 process_cpu_usage_gauge.labels(process_name=process_name).set(process_cpu_usage)
                 process_memory_usage_gauge.labels(process_name=process_name).set(process_memory_usage)
                 process_memory_percentage_usage_gauge.labels(process_name=process_name).set(process_memory_usage_percentage)
-
+                print("\n============================================================================================")
                 print(f"{'System Specification:':<30} Memory: {total_memory:.2f} MB {'':<12}   | System CPU Cores: {total_cores}")
-                print(f"{'System Usage:':<30} Memory Usage: {memory_usage:.2f}%  {'':<12}  | CPU Usage: {cpu_usage:.2f}%")
-                print(f"{process_name + ':(pid = '+ pid +')':<30} Memory Usage: {process_memory_usage:.2f} MB ({process_memory_usage_percentage:.2f}%) {'':<3} | CPU Usage: {process_cpu_usage:.2f}%\n")
+                print(f"{'System Usage:':<30} Memory Usage: {memory_usage:.2f}%  {'':<12} | CPU Usage: {cpu_usage:.2f}%")
+                print(f"{process_name + ':(pid = '+ pid +')':<30} Memory Usage: {process_memory_usage:.2f} MB ({process_memory_usage_percentage:.2f}%) {'':<3} | CPU Usage: {process_cpu_usage:.2f}%")
+                print("Network(i/o):")
+                
+                # Loop through each network interface
+                for iface in net_ifaces:
+                    # Get the network interface statistics
+                    io_stats = psutil.net_io_counters(pernic=True)[iface]
+
+                    # Calculate the I/O rates
+                    bytes_sent = io_stats.bytes_sent - net_io_stats[iface]['bytes_sent']
+                    bytes_recv = io_stats.bytes_recv - net_io_stats[iface]['bytes_recv']
+
+                    # Update the stats dictionary
+                    net_io_stats[iface]['bytes_sent'] = io_stats.bytes_sent
+                    net_io_stats[iface]['bytes_recv'] = io_stats.bytes_recv
+
+                    # Calculate the I/O rates in KB/s
+                    bytes_sent_kb = bytes_sent / 1024
+                    bytes_recv_kb = bytes_recv / 1024
+
+                    if iface == "enp2s0":
+                        net_io_sent_guage.set(bytes_sent_kb)
+                        net_io_recv_guage.set(bytes_recv_kb)
+                    # Print the network I/O stats
+                    # print(f"\t{iface}: {'':<10} {bytes_sent_kb:.2f} KB/s sent {'':<10}| {bytes_recv_kb:.2f} KB/s received")
+                    print(f"\t{iface}: {bytes_sent_kb:.2f} KB/s sent, {bytes_recv_kb:.2f} KB/s received")
+
             except (psutil.NoSuchProcess, psutil.AccessDenied):
                 print(f"Process {process_name} not found or access denied.")
         else:
